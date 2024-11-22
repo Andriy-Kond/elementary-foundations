94fc96d815895e7dc757c157bbbb259137e77ba4
Andrii <andrii.kondratovich@gmail.com>
2024-11-22 10:31:39 +0200

    upd: game other way

--- consoleApp.js
+++ consoleApp.js
@@ -18,24 +18,41 @@ import { promises as fs } from "fs";
 // fs.rename(oldPath, newPath) – перейменування файлу.
 // fs.unlink(path, callback) – видалення файлу.
 
-program.option(
-  "-f, --file [type]", // визначення запуску програми у вигляді: node consoleApp.js -f my_log.txt
-  "file for saving game results",
-  "results.txt", // водночас вказую третім параметром у program.option, що якщо параметр -f не буде переданий у запуску, то за замовчуванням program.file буде дорівнювати results.txt
-);
-
-program.parse(process.argv);
-
+// Налаштування readline для того, щоб спілкуватись з командним рядком:
 const rl = readline.createInterface({
   input: process.stdin, // введення зі стандартного потоку
   output: process.stdout, // виведення у стандартний потік
 });
 
-let count = 0; // кількість спроб
-const logFileName = program.opts().file; // logFile – ім'я файлу, куди будуть збережені результати гри
-const mind = Math.floor(Math.random() * 10) + 1; // загадане число
+// Один .option() на один аргумент. Якщо треба ще один аргумент, то треба написати ще один .option()
+program.option(
+  "-f, --file <type>", // визначення запуску програми у вигляді: node consoleApp -f superLog.txt (можна ще робити інші дужки - [type], але тоді не буде підказки, якщо ввести рядок без вказування імені файлу (node consoleApp -f) - просто при збереженні результату видасть помилку про невдале збереження результату). Якщо ж написати "<type>", то видасть попередження: "error: option '-f, --file <type>' argument missing"
+  "file for saving game results", // коментар до файлу
+  "game_results.txt", // ім'я файлу. якщо параметр -f не буде переданий у запуску, то за замовчуванням program.file буде дорівнювати results.txt
+);
+program.parse(process.argv); // розпарсити передані аргументи - бере з командного рядку передані мною опції і обробляє їх.
 
+// // .on() - це як event listener у React
+// // line - це подія, яка означає, що прийшов якийсь новий рядок (новий ввід)
+// rl.on("line", txt => {
+//   console.log(`You have entered ${txt}!`);
+
+//   process.exit(); // завершує діалог
+// });
+
+// =========== Game ==============
+let count = 0; // кількість спроб (counter of user attempts)
+const mind = Math.ceil(Math.random() * 10); // загадане число (guessed number)
+const logFileName = program.opts().file; // logFileName – файл, куди будуть збережені результати гри. .file - це назва того файлу, що вказується в аргументі --file (-f).
+// або: Math.floor(Math.random() * 10) + 1;)
+// Math.random() - повертає випадкове число від 0 до 1: 0.135456482, 0.78948341532484
+// ceil - округлення у бік збільшення: 1.35, 8.75 => 2, 9
+// floor - округлення у бік зменшення: 1.35, 8.75 => 1, 8
+// round - округлення до найближчого: 5.95, 5.5, 5.05 => 6, 6, 5; -5.05, -5.5, -5.95 => -5, -5, -6
+
+// Input value validator
 const isValid = value => {
+  // Number.isNaN(value) === isNaN(value)
   if (isNaN(value)) {
     console.log("Це виглядає як рядок. Введіть число.".red);
     return false;
@@ -54,34 +71,49 @@ const isValid = value => {
   return true;
 };
 
-const createLogFile = async data => {
+// Всі асинхронні функції треба загортати у try...catch
+const logger = async data => {
   try {
-    await fs.appendFile(logFileName, `${data}\n`);
-    console.log(`Вдалося зберегти результат у файл ${logFileName}`.green);
+    await fs.appendFile(logFileName, `${data}\n`); // записуємо дані (data) у файл
+
+    console.log(
+      `Вдалося зберегти результат у файл ${logFileName} (Successfully game result to game log file ${logFileName})`
+        .green,
+    );
   } catch (err) {
-    console.log(`Не вдалося зберегти файл ${logFileName}`.red);
+    console.log(
+      `Не вдалося зберегти файл ${logFileName}. Something went wrong: ${err.message}`
+        .red,
+    );
   }
 };
 
 const game = () => {
+  // .question() - це як інтерактивний promt у браузері
   rl.question(
-    "Введіть ціле число від 1 до 10, щоб вгадати задумане: ".yellow,
+    "Введіть ціле число від 1 до 10, щоб вгадати задумане:\n".yellow,
     value => {
-      let a = +value;
-      if (!isValid(a)) {
+      let number = +value; // Number(value)
+
+      // validation
+      if (!isValid(number)) {
         game();
         return;
       }
-
       count += 1;
 
-      if (a === mind) {
+      if (number === mind) {
         console.log("Вітаю, Ви вгадали число за %d крок(ів)".green, count);
 
-        createLogFile(
-          `${new Date().toLocaleDateString()}: Вітаю, Ви вгадали число за ${count} крок(ів)`,
-        ).finally(() => rl.close());
-
+        logger(
+          `${new Date().toLocaleString(
+            "uk-UA",
+          )}: Вітаю, Ви вгадали число за ${count} крок(ів)`,
+        );
+        // Можна або додати до logger .finally:
+        // .finally(() => rl.close());
+        // або просто поставити .close():
+        rl.close();
         return;
       }
 
---
+++ index.js
@@ -0,0 +1,3 @@
+// console.log("process:::", process);
+// console.log("process.env:::", process.env); // різні змінні. Можна додавати свої змінні.
+console.log("process.argv:::", process.argv); // шлях до node.exe та файлу виконання (наприклад, index.js)
